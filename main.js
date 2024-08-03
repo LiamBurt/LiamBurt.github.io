ee6527ebbbceec916b46c4de8736ba12ea205819
ElleBurt <raxrevolt@gmail.com>
2024-08-03 13:01:57 +0100

    js order tweak

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-import './style.css'
+
 
 
 
 $(document).ready(function() {
-    document.querySelector('#app').innerHTML = `
-    <div id="splash">
-        <h1>Splash</h1>
-    </div>
     
-    `
   });
+
+document.querySelector('#app').innerHTML = `
+
+    <div id="splash">
+
+        <h1>Splash</h1>
+
+    </div>
+
+`
