a5cc1f4db828d53c628bcd8023893ebc5a3cc0f8
ElleBurt <raxrevolt@gmail.com>
2024-08-03 15:05:03 +0100

    nav bar config

--- main.js
+++ main.js
@@ -4,13 +4,19 @@
 
 $(document).ready(function() {
     
-  });
+});
 
 document.querySelector('#app').innerHTML = `
 
     <div class="nav">
         <img id="logo" src="images/github-logo.png"/>
-        <ul id="nav-bar"></ul>
+        <ul id="nav-bar">
+            <li id="nav-link">About</li>
+            <li id="nav-spacer"> / </li>
+            <li id="nav-link">Blockbench</li>
+            <li id="nav-spacer"> / </li>
+            <li id="nav-link">Mods</li>
+        </ul>
     </div>
 
 `
