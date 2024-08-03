959661b3fdbde6c2c806027de066f16b08c07b8e
ElleBurt <raxrevolt@gmail.com>
2024-08-03 12:59:58 +0100

    css in js

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-
+import './style.css'
 
 
 
 $(document).ready(function() {
-    console.log('working?');
     document.querySelector('#app').innerHTML = `
     <div id="splash">
         <h1>Splash</h1>
